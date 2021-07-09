323f39e9bb82f7ef2fdc1ead8b639a197e1a766d
Yeshashwini17 <87161036+Yeshashwini17@users.noreply.github.com>
2021-07-09 08:53:42 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; MS AIRLINE. All Rights Reserved  |  Contact Us: +91 1111111111</p>	
+        </center>
+    </div>
+</footer>
